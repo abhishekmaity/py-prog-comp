746001ca167cfdcb6b4edd0de86e230d4a941691
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2018-03-02 00:32:44 +0530

    Add files via upload

---
+++ Exhaustive Enumeration.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+3.1 Exhaustive Enumeration
+
+"""
+#Find the cube root of a perfect cube
+x = int(input('Enter an integer: '))
+ans = 0
+while ans**3 < abs(x):
+    ans = ans + 1
+if ans**3 != abs(x):
+    print(x, 'is not a perfect cube')
+else:
+    if x < 0:
+        ans = -ans
+    print('Cube root of', x,'is', ans)
