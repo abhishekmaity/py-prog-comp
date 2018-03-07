d70b0c13d5212fa936f0e89270237700c5b169f9
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2018-03-07 10:54:11 +0530

    Create exe2.2.py

---
+++ exercises/exe2.2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Abhishek
+"""
+#Replace the comment in the following code with a while loop.
+numXs = int(input('How many times should I print the letter X? '))
+toPrint = ''
+#concatenate X to toPrint numXs times
+while numXs!=0:
+    print(toPrint)
+    numXs=numXs-1
