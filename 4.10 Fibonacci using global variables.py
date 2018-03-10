49f81c0903443e32d6c337c62773ef9e3a6933f8
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2018-03-10 23:59:10 +0530

    Create 4.10 Fibonacci using global variables.py

---
+++ 4.10 Fibonacci using global variables.py
@@ -0,0 +1,20 @@
+"""
+4.10 Fibonacci using global variables
+"""
+#4.10 Fibonacci
+def fib(x):
+    """Assumes x an int >= 0
+       Returns Fibonacci of x"""
+    global numFibCalls
+    numFibCalls += 1
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+
+def testFib(n):
+    for i in range(n+1):
+        global numFibCalls
+        numFibCalls = 0
+        print('fib of', i, '=', fib(i))
+        print('fib called', numFibCalls, 'times.')
