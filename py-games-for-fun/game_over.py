1e94100caf825598497b0432298437dca192dbd6
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2018-04-17 11:21:47 +0530

    Create game_over.py

---
+++ py-games-for-fun/game_over.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 14 22:59:35 2018
+
+@author: Abhishek
+
+The "Game Over" game
+"""
+
+# Game Over - Version 2
+#Demonstrates the use of quotes in the strings
+
+print("Program 'Game Over' 2.0")
+
+print("Same", "message", "as before")
+
+print("Just",
+      "a bit",
+      "bigger")
+print("Here", end=" ")
+print("Is is ...")
+
+print(
+        """
+         _____       ___       ___  ___   _____  
+        /  ___|     /   |     /   |/   | |  ___| 
+        | |        / /| |    / /|   /| | | |__   
+        | |  _    / ___ |   / / |__/ | | |  __|  
+        | |_| |  / /  | |  / /       | | | |___  
+        \_____/ /_/   |_| /_/        |_| |_____|
+         
+         _____   _     _   _____   _____   
+        /  _  \ | |   / / |  ___| |  _  \  
+        | | | | | |  / /  | |__   | |_| |  
+        | | | | | | / /   |  __|  |  _  /  
+        | |_| | | |/ /    | |___  | | \ \  
+        \_____/ |___/     |_____| |_|  \_\
+
+        """
+     )
+
+input("\n\nPress the enter key to exit.")
+# Fancy Credits
+# Demonstrates escape sequences
+print("\t\t\tFancy Credits")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tMichael Dawson")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\nSpecial thanks goes out to:")
+print("My hair stylist, Henry \'The Great,\' who never says \"can\'t.\"")
+# sound the system bell
+print("\a")
+input("\n\nPress the enter key to exit.")
+
+# Silly Strings
+# Demonstrates string concatenation and repetition
+print("You can concatenate two " + "strings with the '+' operator.")
+print("\nThis string " + "may not " + "seem terr" + "ibly impressive. " \
++ "But what " + "you don't know" + " is that\n" + "it's one real" \
++ "l" + "y" + " long string, created from the concatenation " \
++ "of " + "twenty-two\n" + "different strings, broken across " \
++ "six lines." + " Now are you" + " impressed? " + "Okay,\n" \
++ "this " + "one " + "long" + " string is now over!")
+print("\nIf you really like a string, you can repeat it. For example,")
+print("who doesn't like pie? That's right, nobody. But if you really")
+print("like it, you should say it like you mean it:")
+print("Pie" * 10)
+input("\n\nPress the enter key to exit.")
+
+# Word Problems
+# Demonstrates numbers and math
+print("If a 2000 pound pregnant hippo gives birth to a 100 pound calf,")
+print("but then eats 50 pounds of food, how much does she weigh?")
+input("Press the enter key to find out.")
+print("2000 - 100 + 50 =", 2000 - 100 + 50)
+print("\nIf an adventurer returns from a successful quest and buys each of")
+print("6 companions 3 bottles of ale, how many bottles are purchased?")
+input("Press the enter key to find out.")
+print("6 * 3 =", 6 * 3)
+print("\nIf a restaurant check comes to 19 dollars with tip, and you and")
+print("your friends split it evenly 4 ways, how much do you each throw in?")
+input("Press the enter key to find out.")
+print("19 / 4 =", 19 / 4)
+print("\nIf a group of 4 pirates finds a chest full of 107 gold coins, and")
+print("they divide the booty evenly, how many whole coins does each get?")
+input("Press the enter key to find out.")
+print("107 // 4 =", 107 // 4)
+print("\nIf that same group of 4 pirates evenly divides the chest full")
+print("of 107 gold coins, how many coins are left over?")
+input("Press the enter key to find out.")
+print("107 % 4 =", 107 % 4)
+input("\n\nPress the enter key to exit.")
